d356485f636e322445baf3adb61af06ba5f51df1
Alexandr <k.v.alexandr@gmail.com>
2020-10-25 04:29:36 +0400

    feat: menu mobile

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,29 @@
+const overlay = document.querySelector('.overlay');
+const headerWrapper = document.querySelector('.header-wrapper');
+const burgerMenu = document.querySelector('.header__burger-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+const viewMenu = () => {
+  headerWrapper.classList.toggle('active');
+  burgerMenu.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  overlay.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
+}
+
+const disableMenu = () => {
+  headerWrapper.classList.remove('active');
+  burgerMenu.classList.remove('active');
+  navMenu.classList.remove('active');
+  overlay.classList.remove('active');
+  document.body.classList.remove('no-scroll');
+}
+
+burgerMenu.addEventListener('click', viewMenu);
+overlay.addEventListener('click', disableMenu);
+
+window.addEventListener('resize', (e) => {
+  if (e.target.innerWidth > 767) {
+    disableMenu();
+  }
+});
